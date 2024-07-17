f5258a5f6fe454430cc641435ad36e07a7cbdce0
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 09:57:50 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <h4 className='text-danger fs-5 text-center'>Browser's details: {navigator.userAgent}</h4>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
