822d0c3e511fc50a9bd6cd0576df99ef338f34c1
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 10:01:14 +0700

    update readme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <div>
-        <h4 className='text-danger fs-5 text-center'>Browser's details: {navigator.userAgent}</h4>
-    </div>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<div>
+    <h4 className='text-danger fs-5 text-center'>Browser's details: {navigator.userAgent}</h4>
+</div>);
